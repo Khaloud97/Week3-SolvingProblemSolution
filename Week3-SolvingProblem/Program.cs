7eaf7cf15dd0f1a059798e22c6650b686534c834
Khaloud <hamedkhaloud71@gmail.com>
2023-11-27 10:48:22 +0400

    Add project files.

---
+++ Week3-SolvingProblem/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Week3_SolvingProblem
+{
+    class Program
+    {
+        static bool IsPalindRome(int x)
+        {
+            string s = x.ToString();//Changed from int to string
+            string revsS = "";
+            for(int i=s.Length-1; i>=0; i--)
+            {
+                revsS += s[i];
+            }
+            if(s== revsS)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool IsPalindRomeOtherWay(int x)
+        {
+            string s = x.ToString();//Changed from int to string
+            int start = 0,end= s.Length - 1;
+
+            while (start <= end)
+            {
+                if (s[start] != s[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            
+            return true;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(IsPalindRome(121));
+            Console.WriteLine(IsPalindRome(-121));
+            Console.WriteLine(IsPalindRome(10));
+            Console.WriteLine(IsPalindRome(-10)); 
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(IsPalindRomeOtherWay(-10));
+            Console.WriteLine(IsPalindRomeOtherWay(121));
+
+
+        }
+    }
+}
